3213afbcc066b45ca9814d36ad73bb4d3d3323f2
rama907 <69743351+rama907@users.noreply.github.com>
2021-03-07 22:57:55 +0700

    Update 2.0

--- Sistem Kendali/Praktikum 3/Kodingan_Sistem_kendali_PWM/Kodingan_Sistem_kendali_PWM.ino
+++ Sistem Kendali/Praktikum 3/Kodingan_Sistem_kendali_PWM/Kodingan_Sistem_kendali_PWM.ino
@@ -6,10 +6,6 @@ int p1=9;
 int p2=6;
 int p3=5;
 
-int led2=8;
-int led3=7;
-int led1=13;
-
 int m1=A0;
 int m2=A1;
 int m3=A2;
@@ -24,10 +20,6 @@ pinMode (p,OUTPUT);
 pinMode (p1,OUTPUT);
 pinMode (p2,OUTPUT);
 pinMode (p3,OUTPUT);
-
-pinMode (led2,OUTPUT);
-pinMode (led3,OUTPUT);
-pinMode (led1,OUTPUT);
 
 pinMode (m1,INPUT);
 pinMode (m2,INPUT);

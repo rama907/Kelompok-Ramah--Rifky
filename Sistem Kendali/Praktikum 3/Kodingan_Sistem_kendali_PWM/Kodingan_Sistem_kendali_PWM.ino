681bd95c10a72cdbe9ccecab5dc325aec1b7a9d1
rama907 <69743351+rama907@users.noreply.github.com>
2021-03-07 21:29:33 +0700

    Update 1.1

---
+++ Sistem Kendali/Praktikum 3/Kodingan_Sistem_kendali_PWM/Kodingan_Sistem_kendali_PWM.ino
@@ -0,0 +1,38 @@
+int p=10;
+int p1=9;
+int p2=6;
+int p3=5;
+
+int led2=8;
+int led3=7;
+int led1=13;
+
+int m1=A0;
+int m2=A1;
+int m3=A2;
+int m4=A3;
+int m5=A4;
+int m6=A5;
+int m7=A6;
+int m8=A7;
+
+void setup() {
+pinMode (p,OUTPUT);
+pinMode (p1,OUTPUT);
+pinMode (p2,OUTPUT);
+pinMode (p3,OUTPUT);
+
+pinMode (led2,OUTPUT);
+pinMode (led3,OUTPUT);
+pinMode (led1,OUTPUT);
+
+pinMode (m1,INPUT);
+pinMode (m2,INPUT);
+pinMode (m3,INPUT);
+pinMode (m4,INPUT);
+pinMode (m5,INPUT);
+pinMode (m6,INPUT);
+pinMode (m7,INPUT);
+pinMode (m8,INPUT);
+
+}
